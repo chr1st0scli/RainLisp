5e1fd684ad8e763da549bb310034b78eb1edc08b
chr1st0scli <chr1st0scli@hotmail.com>
2022-12-05 22:56:44 +0200

    Add the basics of the terminal app.

--- RainLispConsole/RainLispIDE.cs
+++ RainLispConsole/RainLispIDE.cs
@@ -8,71 +8,127 @@ namespace RainLispConsole
     {
         private readonly TextView _inputTextView;
         private readonly TextView _outputTextView;
-        private readonly StatusBar _statusBar;
         private readonly Interpreter _interpreter;
-        private IEvaluationEnvironment? _environment;
 
         public RainLispIDE()
         {
             _interpreter = new();
 
-            Title = "RainLisp";
+            Title = "Rainλisp";
+
+            var textViewColorScheme = new ColorScheme()
+            {
+                Focus = new Terminal.Gui.Attribute(Color.White, Color.Black),
+            };
 
             _inputTextView = new()
             {
                 Width = Dim.Fill(),
-                Height = Dim.Percent(50),
+                Height = Dim.Fill(),
+                ColorScheme = textViewColorScheme
             };
 
             _outputTextView = new()
             {
                 ReadOnly = true,
-                Y = Pos.Bottom(_inputTextView) + 1,
                 Width = Dim.Fill(),
-                Height = Dim.Height(_inputTextView) - 2,
+                Height = Dim.Fill(),
+                ColorScheme = textViewColorScheme
+            };
+
+            var inputFrameView = new FrameView("Code Editor")
+            {
+                Y = 1,  // Leave a row for the menu.
+                Width = Dim.Fill(),
+                Height = Dim.Percent(70)
+            };
+
+            var outputFrameView = new FrameView("Output")
+            {
+                Y = Pos.Bottom(inputFrameView) + 1,
+                Width = Dim.Fill(),
+                Height = Dim.Fill(1),   // Leave a row for the status bar.
             };
 
             StatusItem[] statusBarItems = new[]
             {
                 new StatusItem(Key.CtrlMask | Key.Enter, "Ctrl-Enter Evaluate", Evaluate),
-                new StatusItem(Key.ShiftMask | Key.F5, "Ctrl-R Reset", Reset),
                 new StatusItem(Key.CtrlMask | Key.q, "Ctrl-Q Quit", () => Application.RequestStop()),
             };
-            _statusBar = new StatusBar(statusBarItems);
+            var statusBar = new StatusBar(statusBarItems);
 
-            Add(_inputTextView, _outputTextView, _statusBar);
+            MenuItem[] fileMenuItems = new[]
+            {
+                new MenuItem(Key.CtrlMask | Key.n) { Title = "New", Action = New },
+                new MenuItem(Key.CtrlMask | Key.o) { Title = "Open", Action = Open },
+                new MenuItem(Key.CtrlMask | Key.s) { Title = "Save", Action = Save }
+            };
+
+            MenuItem[] helpMenuItems = new[]
+            {
+                new MenuItem(Key.F1) { Title = "View Help", Action = ViewHelp },
+                new MenuItem() { Title = "About", Action = About }
+            };
+
+            MenuBarItem[] menuBarItems = new[]
+            {
+                new MenuBarItem("File", fileMenuItems),
+                new MenuBarItem("Help", helpMenuItems),
+            };
+
+            var menuBar = new MenuBar(menuBarItems);
+
+            inputFrameView.Add(_inputTextView);
+            outputFrameView.Add(_outputTextView);
+            Add(menuBar, inputFrameView, outputFrameView, statusBar);
+            MenuBar = menuBar;
+        }
+
+        private void New()
+        {
+
+        }
+
+        private void Open()
+        {
+
+        }
+
+        private void Save()
+        {
+
+        }
+
+        private void ViewHelp()
+        {
+
+        }
+
+        private void About()
+        {
+            MessageBox.Query("About", Resources.ABOUT, "OK");
         }
 
         private void Evaluate()
         {
-            string? expression = _inputTextView.Text.ToString();
-            _inputTextView.Text = string.Empty;
-            _interpreter.EvaluateAndPrint(expression, ref _environment, Print, PrintError);
-        }
-
-        private void Reset()
-        {
-            _environment = null;
-            _outputTextView.Text = "Environment has been reset.";
+            IEvaluationEnvironment? environment = null;
+            _interpreter.EvaluateAndPrint(_inputTextView.Text.ToString(), ref environment, Print, PrintError);
         }
 
         private void Print(string result)
-            => AppendToOutput(result);
+            => _outputTextView.Text = result;
 
         private void PrintError(string message, Exception ex)
         {
-            AppendToOutput(message);
+            _outputTextView.Text = message;
 
             // Print the entire exception if it is unknown.
             if (message == ErrorMessages.UNKNOWN_ERROR)
-                AppendToOutput(ex.ToString());
+                _outputTextView.Text = ex.ToString();
 
             // Or print the exception's message if one provided by a programmer extending the library.
             else if (!string.IsNullOrWhiteSpace(ex.Message) && !ex.Message.StartsWith("Exception of type"))
-                AppendToOutput(ex.Message);
+                _outputTextView.Text = ex.Message;
         }
-
-        private void AppendToOutput(string line)
-            => _outputTextView.Text += Environment.NewLine + line;
     }
 }
