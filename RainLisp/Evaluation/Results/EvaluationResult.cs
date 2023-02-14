26ff1a4ef98b4a6bc34ae027f0d58c9a49922b0d
chr1st0scli <chr1st0scli@gmail.com>
2023-02-14 22:05:07 +0200

    Make the = primitive function support more than just primitive data types.

--- RainLisp/Evaluation/Results/EvaluationResult.cs
+++ RainLisp/Evaluation/Results/EvaluationResult.cs
@@ -1,10 +1,13 @@
 ﻿namespace RainLisp.Evaluation.Results
 {
-    public abstract class EvaluationResult
+    public abstract class EvaluationResult : IEquatable<EvaluationResult>
     {
         public abstract T AcceptVisitor<T>(IEvaluationResultVisitor<T> visitor);
 
         public virtual EvaluationResult AcceptVisitor(IProcedureApplicationVisitor visitor, EvaluationResult[]? evaluatedArguments, IEvaluatorVisitor evaluatorVisitor)
             => throw new NotProcedureException();
+
+        public virtual bool Equals(EvaluationResult? other)
+            => base.Equals(other);
     }
 }
--- RainLisp/Evaluation/Results/PrimitiveDatum.cs
+++ RainLisp/Evaluation/Results/PrimitiveDatum.cs
@@ -8,5 +8,13 @@
         public T Value { get; init; }
 
         public object GetValueAsObject() => Value;
+
+        public override bool Equals(EvaluationResult? other)
+        {
+            if (other is PrimitiveDatum<T> datum)
+                return Value.Equals(datum.Value);
+
+            return base.Equals(other);
+        }
     }
 }
