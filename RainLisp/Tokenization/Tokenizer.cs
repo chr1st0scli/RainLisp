d598ba63f6ab4fe567e9721161438108b1a51737
chr1st0scli <chr1st0scli@gmail.com>
2023-02-27 01:45:17 +0200

    Decrease tokenizer's cyclomatic complexity.

--- RainLisp/Tokenization/Tokenizer.cs
+++ RainLisp/Tokenization/Tokenizer.cs
@@ -7,191 +7,202 @@ namespace RainLisp.Tokenization
 {
     public class Tokenizer : ITokenizer
     {
+        private uint _line;
+        private uint _charPosition;
+        private bool _charInstring;
+        private bool _charInNumber;
+        private bool _charInComment;
+        private StringTokenizer? _stringTokenizer;
+        private NumberTokenizer? _numberTokenizer;
+        private StringBuilder _lexemeStringBuilder = null!;
+        private IList<Token> _tokens = null!;
+
         public IList<Token> Tokenize(string? expression)
         {
-            var tokens = new List<Token>();
-            uint line = 1, charPosition = 1;
+            _tokens = new List<Token>();
+            _line = _charPosition = 1;
 
             if (string.IsNullOrEmpty(expression))
             {
                 RegisterEOF();
-                return tokens;
+                return _tokens;
             }
 
-            bool charInstring = false, charInComment = false, charInNumber = false;
-            var lexemeStringBuilder = new StringBuilder();
-            StringTokenizer? stringTokenizer = null;
-            NumberTokenizer? numberTokenizer = null;
-
-            #region Local Helper Methods.
-            void RegisterToken(Token token)
-                => tokens.Add(token);
-
-            void RegisterSpecificToken(string value, TokenType tokenType, uint position, double numberValue = 0, bool booleanValue = false, string stringValue = "")
-                => RegisterToken(new() { Value = value, Type = tokenType, Line = line, Position = position, NumberValue = numberValue, BooleanValue = booleanValue, StringValue = stringValue });
-
-            void RegisterEOF()
-                => RegisterSpecificToken(string.Empty, TokenType.EOF, charPosition);
-
-            void RegisterStringLiteralToken()
-            {
-                charInstring = false;
-                RegisterSpecificToken(stringTokenizer!.GetStringLiteral(), TokenType.String, GetLastStringStartPosition(), stringValue: stringTokenizer.GetStringValue());
-                stringTokenizer.Clear();
-            }
-
-            void RegisterUnknownToken()
-            {
-                if (lexemeStringBuilder.Length == 0)
-                    return;
-
-                string value = lexemeStringBuilder.ToString();
-                lexemeStringBuilder.Clear();
-
-                if (string.IsNullOrWhiteSpace(value))
-                    return;
-
-                if (charInNumber)
-                {
-                    charInNumber = false;
-                    RegisterSpecificToken(value, TokenType.Number, charPosition - (uint)value.Length, numberTokenizer!.GetNumber());
-                    numberTokenizer.Clear();
-                }
-                else
-                    RegisterToken(InferToken(value, line, charPosition - (uint)value.Length));
-            }
-
-            void ChangeLine()
-            {
-                line++;
-                charPosition = 1;
-            }
-
-            void PlatformBounceNextNewLine(ref int i)
-            {
-                // Skip the next \n character, so that \r\n is treated as a single new line if the platform dictates it.
-                if (i < expression.Length - 1 && expression[i + 1] == NEW_LINE && Environment.NewLine == $"{CARRIAGE_RETURN}{NEW_LINE}")
-                    i++;
-            }
-
-            bool NumberStartsAt(int i)
-            {
-                // If a lexeme is already being built, we are not dealing with the start.
-                if (lexemeStringBuilder.Length > 0)
-                    return false;
-
-                char c = expression[i];
-
-                // If it starts with a digit, then a number is being started.
-                if (char.IsDigit(c))
-                    return true;
-
-                // If it starts with a number sign followed by a digit, then a number is considered to being started.
-                if ((c == PLUS || c == MINUS) && i < expression.Length - 1 && char.IsDigit(expression[i + 1]))
-                    return true;
-
-                return false;
-            }
-
-            // The string token's position relates to the actual number of characters typed in the string and not the resulting string value's length.
-            uint GetLastStringStartPosition()
-                => charPosition - stringTokenizer!.CharactersProcessed - 1;
-            #endregion
+            _charInstring = _charInNumber = _charInComment = false;
+            _stringTokenizer = null;
+            _numberTokenizer = null;
+            _lexemeStringBuilder = new StringBuilder();
 
             for (int i = 0; i < expression.Length; i++)
-            {
-                char c = expression[i];
+                ProcessCharacter(expression[i], ref i, expression);
 
-                if (charInstring)
-                    stringTokenizer!.AddToString(c, line, charPosition);
-
-                // Disregard a character in a comment except a new line that ends it.
-                else if (charInComment)
-                {
-                    if (c == CARRIAGE_RETURN)
-                    {
-                        charInComment = false;
-                        ChangeLine();
-                        PlatformBounceNextNewLine(ref i);
-                    }
-                    else if (c == NEW_LINE)
-                    {
-                        charInComment = false;
-                        ChangeLine();
-                    }
-                    continue; // Skip advancing character position.
-                }
-                // Start of a comment.
-                else if (c == COMMENT)
-                {
-                    RegisterUnknownToken();
-                    charInComment = true;
-                    continue; // Skip advancing character position.
-                }
-                // Start of a string.
-                else if (c == DOUBLE_QUOTE)
-                {
-                    RegisterUnknownToken();
-                    charInstring = true;
-                    stringTokenizer ??= new StringTokenizer(RegisterStringLiteralToken);
-                }
-                else if (c == LPAREN)
-                {
-                    RegisterUnknownToken();
-                    RegisterSpecificToken(c.ToString(), TokenType.LParen, charPosition);
-                }
-                else if (c == RPAREN)
-                {
-                    RegisterUnknownToken();
-                    RegisterSpecificToken(c.ToString(), TokenType.RParen, charPosition);
-                }
-                else if (c == SINGLE_QUOTE)
-                {
-                    RegisterUnknownToken();
-                    RegisterSpecificToken(c.ToString(), TokenType.QuoteAlt, charPosition);
-                }
-                else if (c == CARRIAGE_RETURN)
-                {
-                    RegisterUnknownToken();
-                    ChangeLine();
-                    PlatformBounceNextNewLine(ref i);
-                    continue; // Skip advancing character position.
-                }
-                else if (c == NEW_LINE)
-                {
-                    RegisterUnknownToken();
-                    ChangeLine();
-                    continue; // Skip advancing character position.
-                }
-                else if (c == SPACE || c == TAB)
-                    RegisterUnknownToken();
-
-                else if (charInNumber)
-                {
-                    numberTokenizer!.AddToNumber(c, line, charPosition);
-                    lexemeStringBuilder.Append(c);
-                }
-                else if (NumberStartsAt(i))
-                {
-                    charInNumber = true;
-                    numberTokenizer ??= new NumberTokenizer();
-                    numberTokenizer.AddToNumber(c, line, charPosition);
-                    lexemeStringBuilder.Append(c);
-                }
-                else
-                    lexemeStringBuilder.Append(c);
-
-                charPosition++;
-            }
-
-            if (charInstring)
-                throw new NonTerminatedStringException(line, GetLastStringStartPosition());
+            if (_charInstring)
+                throw new NonTerminatedStringException(_line, GetLastStringStartPosition());
 
             RegisterUnknownToken();
             RegisterEOF();
 
-            return tokens;
+            return _tokens;
         }
+
+        private void ProcessCharacter(char c, ref int i, string expression)
+        {
+            if (_charInstring)
+                _stringTokenizer!.AddToString(c, _line, _charPosition);
+
+            else if (_charInComment)
+            {
+                ProcessCommentCharacter(c, ref i, expression);
+                return; // Skip advancing character position.
+            }
+            // Start of a comment.
+            else if (c == COMMENT)
+            {
+                RegisterUnknownToken();
+                _charInComment = true;
+                return; // Skip advancing character position.
+            }
+            // Start of a string.
+            else if (c == DOUBLE_QUOTE)
+            {
+                RegisterUnknownToken();
+                _charInstring = true;
+                _stringTokenizer ??= new StringTokenizer(RegisterStringLiteralToken);
+            }
+            else if (c == LPAREN)
+            {
+                RegisterUnknownToken();
+                RegisterSpecificToken(c.ToString(), TokenType.LParen, _charPosition);
+            }
+            else if (c == RPAREN)
+            {
+                RegisterUnknownToken();
+                RegisterSpecificToken(c.ToString(), TokenType.RParen, _charPosition);
+            }
+            else if (c == SINGLE_QUOTE)
+            {
+                RegisterUnknownToken();
+                RegisterSpecificToken(c.ToString(), TokenType.QuoteAlt, _charPosition);
+            }
+            else if (c == CARRIAGE_RETURN)
+            {
+                RegisterUnknownToken();
+                ChangeLine();
+                PlatformBounceNextNewLine(ref i, expression);
+                return; // Skip advancing character position.
+            }
+            else if (c == NEW_LINE)
+            {
+                RegisterUnknownToken();
+                ChangeLine();
+                return; // Skip advancing character position.
+            }
+            else if (c == SPACE || c == TAB)
+                RegisterUnknownToken();
+
+            else if (_charInNumber)
+            {
+                _numberTokenizer!.AddToNumber(c, _line, _charPosition);
+                _lexemeStringBuilder.Append(c);
+            }
+            else if (NumberStartsAt(i, expression))
+            {
+                _charInNumber = true;
+                _numberTokenizer ??= new NumberTokenizer();
+                _numberTokenizer.AddToNumber(c, _line, _charPosition);
+                _lexemeStringBuilder.Append(c);
+            }
+            else
+                _lexemeStringBuilder.Append(c);
+
+            _charPosition++;
+        }
+
+        private void ProcessCommentCharacter(char c, ref int i, string expression)
+        {
+            // Disregard a character in a comment except a new line that ends it.
+            if (c == CARRIAGE_RETURN)
+            {
+                _charInComment = false;
+                ChangeLine();
+                PlatformBounceNextNewLine(ref i, expression);
+            }
+            else if (c == NEW_LINE)
+            {
+                _charInComment = false;
+                ChangeLine();
+            }
+        }
+
+        private void RegisterToken(Token token)
+            => _tokens.Add(token);
+
+        private void RegisterSpecificToken(string value, TokenType tokenType, uint position, double numberValue = 0, bool booleanValue = false, string stringValue = "")
+            => RegisterToken(new() { Value = value, Type = tokenType, Line = _line, Position = position, NumberValue = numberValue, BooleanValue = booleanValue, StringValue = stringValue });
+
+        private void RegisterEOF()
+            => RegisterSpecificToken(string.Empty, TokenType.EOF, _charPosition);
+
+        private void RegisterStringLiteralToken()
+        {
+            _charInstring = false;
+            RegisterSpecificToken(_stringTokenizer!.GetStringLiteral(), TokenType.String, GetLastStringStartPosition(), stringValue: _stringTokenizer.GetStringValue());
+            _stringTokenizer.Clear();
+        }
+
+        private void RegisterUnknownToken()
+        {
+            if (_lexemeStringBuilder.Length == 0)
+                return;
+
+            string value = _lexemeStringBuilder.ToString();
+            _lexemeStringBuilder.Clear();
+
+            if (_charInNumber)
+            {
+                _charInNumber = false;
+                RegisterSpecificToken(value, TokenType.Number, _charPosition - (uint)value.Length, _numberTokenizer!.GetNumber());
+                _numberTokenizer.Clear();
+            }
+            else
+                RegisterToken(InferToken(value, _line, _charPosition - (uint)value.Length));
+        }
+
+        private void ChangeLine()
+        {
+            _line++;
+            _charPosition = 1;
+        }
+
+        private static void PlatformBounceNextNewLine(ref int i, string expression)
+        {
+            // Skip the next \n character, so that \r\n is treated as a single new line if the platform dictates it.
+            if (i < expression.Length - 1 && expression[i + 1] == NEW_LINE && Environment.NewLine == $"{CARRIAGE_RETURN}{NEW_LINE}")
+                i++;
+        }
+
+        private bool NumberStartsAt(int i, string expression)
+        {
+            // If a lexeme is already being built, we are not dealing with the start of a number.
+            if (_lexemeStringBuilder.Length > 0)
+                return false;
+
+            char c = expression[i];
+
+            // If it starts with a digit, then a number is being started.
+            if (char.IsDigit(c))
+                return true;
+
+            // If it starts with a number sign followed by a digit, then a number is considered to being started.
+            if ((c == PLUS || c == MINUS) && i < expression.Length - 1 && char.IsDigit(expression[i + 1]))
+                return true;
+
+            return false;
+        }
+
+        // The string token's position relates to the actual number of characters typed in the string and not the resulting string value's length.
+        private uint GetLastStringStartPosition()
+            => _charPosition - _stringTokenizer!.CharactersProcessed - 1;
 
         private static Token InferToken(string value, uint line, uint position)
         {
