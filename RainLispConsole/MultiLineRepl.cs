64da3cab198c7cbc30513adcd4714b8622975998
chr1st0scli <chr1st0scli@hotmail.com>
2023-05-28 21:39:27 +0300

    Editing in multiline REPL mode.

--- RainLispConsole/MultiLineRepl.cs
+++ RainLispConsole/MultiLineRepl.cs
@@ -15,6 +15,7 @@ namespace RainLispConsole
         {
             Console.WriteLine(Resources.REPL_MULTILINE_PROMPT);
             var lineBuilder = new StringBuilder();
+            _lines.Add("");
 
             int currentLine = 0;
             bool terminate = false;
@@ -24,13 +25,20 @@ namespace RainLispConsole
 
                 if (keyInfo.Key == ConsoleKey.Enter)
                 {
-                    currentLine++;
-                    _lines.Add(lineBuilder.ToString());
+                    _lines[currentLine] = lineBuilder.ToString();
                     lineBuilder.Clear();
-                    Console.WriteLine();
 
                     if ((keyInfo.Modifiers & ConsoleModifiers.Control) == ConsoleModifiers.Control)
                         terminate = true;
+                    else
+                    {
+                        currentLine++;
+                        if (currentLine == _lines.Count)
+                            _lines.Add("");
+                        else
+                            _lines.Insert(currentLine, "");
+                        Console.WriteLine();
+                    }
                 }
                 else if (keyInfo.Key == ConsoleKey.LeftArrow)
                 {
@@ -46,16 +54,32 @@ namespace RainLispConsole
                 {
                     if (currentLine > 0)
                     {
+                        _lines[currentLine] = lineBuilder.ToString();
+                        lineBuilder.Clear();
+
                         Console.CursorTop--;
                         currentLine--;
+
+                        lineBuilder.Append(_lines[currentLine]);
+
+                        if (Console.CursorLeft > lineBuilder.Length)
+                            Console.CursorLeft = lineBuilder.Length;
                     }
                 }
                 else if (keyInfo.Key == ConsoleKey.DownArrow)
                 {
                     if (currentLine < _lines.Count - 1)
                     {
+                        _lines[currentLine] = lineBuilder.ToString();
+                        lineBuilder.Clear();
+
                         Console.CursorTop++;
                         currentLine++;
+
+                        lineBuilder.Append(_lines[currentLine]);
+
+                        if (Console.CursorLeft > lineBuilder.Length)
+                            Console.CursorLeft = lineBuilder.Length;
                     }
                 }
                 else if (keyInfo.Key == ConsoleKey.Backspace)
@@ -85,10 +109,28 @@ namespace RainLispConsole
                 }
                 else if (!char.IsControl(keyInfo.KeyChar))
                 {
-                    lineBuilder.Append(keyInfo.KeyChar);
-                    Console.Write(keyInfo.KeyChar);
+                    if (Console.CursorLeft == lineBuilder.Length)
+                    {
+                        lineBuilder.Append(keyInfo.KeyChar);
+                        Console.Write(keyInfo.KeyChar);
+                    }
+                    else
+                    {
+                        int cursorPos = Console.CursorLeft;
+                        lineBuilder.Insert(cursorPos, keyInfo.KeyChar);
+                        Console.Write(lineBuilder.ToString(cursorPos, lineBuilder.Length - cursorPos));
+                        Console.CursorLeft = cursorPos + 1;
+                    }
                 }
             } while (!terminate);
+
+            if (currentLine < _lines.Count - 1)
+                Console.CursorTop += _lines.Count - currentLine - 1;
+
+            if (Console.CursorLeft < _lines[^1].Length)
+                Console.CursorLeft = _lines[^1].Length;
+
+            Console.WriteLine();
 
             var sb = new StringBuilder().AppendJoin(Environment.NewLine, _lines);
             _lines.Clear();
