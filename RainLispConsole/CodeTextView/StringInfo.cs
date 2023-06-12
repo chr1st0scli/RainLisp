7407d381cd7bb7e48366a361de66069b0c4327f3
chr1st0scli <chr1st0scli@gmail.com>
2023-06-12 21:51:38 +0300

    Remove console app that was moved to another repository.

--- RainLispConsole/CodeTextView/StringInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RainLispConsole.CodeTextView
-{
-    internal readonly struct StringInfo
-    {
-        public int Start { get; init; }
-
-        public int End { get; init; }
-    }
-}
